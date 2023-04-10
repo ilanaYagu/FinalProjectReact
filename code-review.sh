87342f5e51aea9bd14cb39819033aeebd7b9174d
ilanaYagu <ilana542002@gmail.com>
2023-04-10 21:49:35 +0300

    commit

--- code-review.sh
+++ code-review.sh
@@ -19,10 +19,17 @@ function code_review {
     # Extract the suggested changes from the OpenAI API response
     suggested_changes=$(echo $response | jq -r '.choices[].text')
 
-    # Print the suggested changes
-    echo "Suggested changes for $file:"
-    echo "$suggested_changes"
+    # Add a comment to the pull request with the suggested changes
+    pr_number=$(echo $GITHUB_REF | cut -d/ -f3)
+    repo=$(basename `git rev-parse --show-toplevel`)
+    api_url="https://api.github.com/repos/$GITHUB_REPOSITORY/issues/$pr_number/comments"
+    body="Suggested changes for \`$file\`: \n\n$suggested_changes"
+    curl -H "Authorization: token $GITHUB_TOKEN" \
+        --header "Content-Type: application/json" \
+        --data "{\"body\":\"$body\"}" \
+        $api_url
 }
+
 
 # Run code review on all JavaScript files in the src directory
 for file in $(find src -type f -name "*.js"); do
