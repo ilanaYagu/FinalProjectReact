5602a655039a049ccb1029f1f65e0fefe21d559b
ilanaYagu <ilana542002@gmail.com>
2023-04-11 21:31:17 +0300

    commit

--- code-review.sh
+++ code-review.sh
@@ -1,42 +1,11 @@
 #!/bin/bash
 
+REPOSITORY_CODE=$(find src -type f -name "*.ts" -o -name "*.tsx" | xargs cat)
+OPENAI_API_KEY=$1
 
-# Install the OpenAI API client
-pip install openai
+curl -s -H "Content-Type: application/json" \
+     -H "Authorization: Bearer $OPENAI_API_KEY" \
+     -d "{\"model\": \"text-davinci-002\", \"prompt\": \"Review the following TypeScript code in a React application:\n\n${REPOSITORY_CODE}\", \"max_tokens\": 200, \"n\": 1, \"stop\": null, \"temperature\": 0.5, \"top_p\": 1, \"frequency_penalty\": 0, \"presence_penalty\": 0}" \
+     -X POST https://api.openai.com/v1/engines/davinci-codex/completions \
+     > review.json
 
-# Define the function to perform code review using the OpenAI API
-function code_review {
-    file=$1
-    model=$2
-
-    echo "Processing file $file with engine $model"
-
-    response=$(openai api completions.create \
-        --engine $model \
-        --prompt "Code review for $file" \
-        --temperature 0.5 \
-        --max-tokens 1024 \
-        --stop "###")
-
-    # Extract the suggested changes from the OpenAI API response
-    suggested_changes=$(echo $response | jq -r '.choices[].text')
-
-echo "Suggested changes: $suggested_changes"
-
-    # Add a comment to the pull request with the suggested changes
-    pr_number=$(echo $GITHUB_REF | cut -d/ -f3)
-    repo=$(basename `git rev-parse --show-toplevel`)
-    api_url="https://api.github.com/repos/$GITHUB_REPOSITORY/issues/$pr_number/comments"
-    body="Suggested changes for \`$file\`: \n\n$suggested_changes"
-    curl -H "Authorization: token $MY_GITHUB_TOKEN" \
-        --header "Content-Type: application/json" \
-        --data "{\"body\":\"$body\"}" \
-        $api_url
-}
-
-
-
-# Run code review on all JavaScript files in the src directory
-for file in $(find src -type f -name "*.js"); do
-    code_review $file "davinci-codex"
-done
